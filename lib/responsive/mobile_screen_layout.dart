1f46e14f704c0d52ab5d79685ea958bc676fd1b3
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2022-07-12 11:02:21 +0500

    signup form

---
+++ lib/responsive/mobile_screen_layout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class MobileScreenLayout extends StatelessWidget {
+  const MobileScreenLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('mobile screen layout')));
+  }
+}
